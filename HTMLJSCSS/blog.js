bbc3d6c849ecefaaa6460a1b7975e3d5a6eac7af
Kaelan Mikowicz <eternity01033@gmail.com>
2017-10-30 13:43:24 -0700

    form method

--- HTMLJSCSS/blog.js
+++ HTMLJSCSS/blog.js
@@ -1,9 +1,11 @@
 
 function addPost()
 {
-    var name = prompt("Name:");
-    var blogpost = prompt("Blog Text:");
+    var name = window.document.getElementById("author").value;
+    var blogpost = window.document.getElementById("post").value;
 
     var mainBlog = document.getElementById("blogs");
     mainBlog.innerHTML += '<article><h1>' + name + '</h1><p>'+ blogpost + '</p></article>';
+    window.document.getElementById("author").value = "";
+    window.document.getElementById("post").value = ""    
 }
